45d99a52171d791f4121dc2c578e2c4d8c66edef
Max <megamax.sk@gmail.com>
2019-10-12 14:02:24 -0700

    Update StringPermutations.java

--- StringPermutations.java
+++ StringPermutations.java
@@ -4,13 +4,14 @@ public class Permutation {
 		permutation("", s); 
 	}
 
-    private static void permutation(String prefix, String s) {
-        int n = s.length();
-        if (n == 0) {
-        	System.out.println(prefix);
-        } else {
-            for (int i = 0; i < n; i++)
-               permutation(prefix + s.charAt(i), s.substring(0, i) + s.substring(i+1));
-        }
+	private static void permutation(String prefix, String s) {
+		int n = s.length();
+		if (n == 0) {
+			System.out.println(prefix);
+		} else {
+			for (int i = 0; i < n; i++) {
+			   permutation(prefix + s.charAt(i), s.substring(0, i) + s.substring(i+1));
+			}
+		}
 	}
 }
